59372630cc81f72163fbf0ff3b281627fb15147a
ajilantang <ajilantang@gmail.com>
2016-08-11 03:03:06 +0700

    math-basic

---
+++ math-basic.js
@@ -0,0 +1,16 @@
+var pangkatAngka = function (x,y){
+  return Math.power (x,y);
+}
+pangkatAngka(2,3);
+var akarKuadrat = function(a) {
+  return Math.pow(a,0.5)
+}
+akarKuadrat(9);
+var hipotenusa = function(p,q) {
+  return Math.pow((p*p+q*q),0.5)+"cm"
+}
+hipotenusa(3,4);
+var luaslingkaran = function (r) {
+  return Math.PI*r*r +" cm"
+}
+luaslingkaran(7);
